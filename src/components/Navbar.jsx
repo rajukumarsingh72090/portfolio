d3b54277721308c8fff99ed1ac6cbf0ef152ece2
Raju <raju@localhost.localdomain>
2025-05-23 20:26:37 +0530

    new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {links} from '../Data'
 import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <nav className="nav">
-      <div className="nav__menu">
+      <div className={`${showMenu ? 'nav__menu show-menu': 'nav__menu'}`}>
         <ul className="nav__list">
           {links.map(({name, icon, path}, index) => {
             return(
               <li className="nav__item" key={index}>
-                <NavLink to={path} className='nav__link'>
+                <NavLink to={path} className={({isActive}) => isActive?
+                'nav__link active-nav': 'nav__link'}>
                   {icon} 
                   <h3 className='nav__name'>{name}</h3>
                 </NavLink>
@@ -20,7 +24,7 @@ const Navbar = () => {
           })}
         </ul>
       </div>
-      <div className='nav__toggle'>
+      <div className={`${showMenu ? 'nav__toggle animate-toggle': 'nav__toggle'}`} onClick={() => setShowMenu(!showMenu)}>
         <span></span>
         <span></span>
         <span></span>
