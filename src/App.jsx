119be079911126deacb66d4ef0fc7a0d45d24d48
Raju Kumar Singh <rajukumarsingh72090@gmail.com>
2025-08-08 22:49:42 +0530

    Update App.jsx
    LinkedIn class added

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ function App() {
     <Route path='portfolio' element={<Portfolio/>}/>
     <Route path='contact' element={<Contact/>}/>
    </Routes>
+
+     <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="raju-kumar-singh-1ab862360" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/raju-kumar-singh-1ab862360?trk=profile-badge">Raju Kumar Singh</a></div>
+              
    </BrowserRouter>
   )
 }
